5508326f97cde33bc548704d1e26b28f6d23f4af
hemanttolwani89 <hemanttolwani1989@gmail.com>
2018-12-29 23:58:45 +0100

    add comment model

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,80 @@
+package ImageHoster.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Size(max = 5000)
+    @Column(name = "text")
+    private String text;
+
+    private LocalDate createdDate;
+    
+    @ManyToOne
+    private User user;
+    
+    @ManyToOne
+    private Image image;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public Comment() {
+		super();
+	}
+}
+
